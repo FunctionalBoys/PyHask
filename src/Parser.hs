78a7d0f44365c3bcd8b0dc6d9d2a3d7ef55e4700
Jose Flores <josecruzflores.fl@gmail.com>
2020-04-02 19:50:23 -0600

    Update reserved words

--- src/Parser.hs
+++ src/Parser.hs
@@ -13,7 +13,7 @@ import qualified Text.Megaparsec.Char.Lexer as L
 type Parser = Parsec Void Text
 
 reservedWords :: [Text]
-reservedWords = ["if", "elif", "else", "for", "while", "let", "def", "class", "True", "False", "continue", "pass", "break"]
+reservedWords = ["if", "elif", "else", "for", "while", "let", "def", "class", "True", "False", "continue", "pass", "break", "and", "or", "not", "print", "read"]
 
 scn :: Parser ()
 scn = L.space space1 empty empty

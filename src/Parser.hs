da78c9178210b1ce28803d2eb8fbe120ee5c70d5
Jose Flores <josecruzflores.fl@gmail.com>
2020-04-02 18:15:57 -0600

    Add Parser's base

---
+++ src/Parser.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Parser where
+
+import           Control.Monad              (void)
+import           Data.Text                  (Text)
+import           Data.Void
+import           Text.Megaparsec
+import           Text.Megaparsec.Char
+import qualified Text.Megaparsec.Char.Lexer as L
+
+type Parser = Parsec Void Text
+
+scn :: Parser ()
+scn = L.space space1 empty empty
+
+sc :: Parser ()
+sc = L.space (void $ some (char ' ' <|> char '\t')) empty empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: Text -> Parser Text
+symbol = L.symbol sc
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+

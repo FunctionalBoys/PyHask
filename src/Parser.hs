51c1c307cad3a2c1ff0dd4c81816b5cc7da8889e
martinrrm <martin.ruiz.mares@gmail.com>
2020-04-02 19:07:04 -0600

    Initial symbols

--- src/Parser.hs
+++ src/Parser.hs
@@ -26,3 +26,68 @@ symbol = L.symbol sc
 parens :: Parser a -> Parser a
 parens = between (symbol "(") (symbol ")")
 
+brackets :: Parser a -> Parser a
+brackets = between (symbol "[") (symbol "]")
+
+ifSymbol :: Parser Text
+ifSymbol = symbol "if"
+
+elifSymbol :: Parser Text
+elifSymbol = symbol "elif"
+
+elseSymbol :: Parser Text
+elseSymbol = symbol "else"
+
+plusSymbol :: Parser Text
+plusSymbol = symbol "+"
+
+minusSymbol :: Parser Text
+minusSymbol = symbol "-"
+
+divisionSymbol :: Parser Text
+divisionSymbol = symbol "/"
+
+timesSymbol :: Parser Text
+timesSymbol = symbol "*"
+
+equalSymbol :: Parser Text
+equalSymbol = symbol ":="
+
+isEqualSymbol :: Parser Text
+isEqualSymbol = symbol "=="
+
+lessSymbol :: Parser Text
+lessSymbol = symbol "<"
+
+greaterSymbol :: Parser Text
+greaterSymbol = symbol ">"
+
+differentSymbol :: Parser Text
+differentSymbol = symbol "!="
+
+lessEqSymbol :: Parser Text
+lessEqSymbol = symbol "<="
+
+greaterEqSymbol :: Parser Text
+greaterEqSymbol = symbol ">="
+
+andSymbol :: Parser Text 
+andSymbol = symbol "and"
+
+orSymbol :: Parser Text
+orSymbol = symbol "or"
+
+notSymbol :: Parser Text
+notSymbol = symbol "not"
+
+commaSymbol :: Parser Text
+commaSymbol = symbol ","
+
+dotSymbol :: Parser Text
+dotSymbol = symbol "."
+
+voidSymbol :: Parser Text
+voidSymbol = symbol "void"
+
+printSymbol :: Parser Text
+printSymbol = symbol "print"

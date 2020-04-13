4954f87c6b5095d59fd3fd60758d6df0b8bdac98
Jose Flores <josecruzflores.fl@gmail.com>
2020-04-12 23:27:28 -0500

    Create entry point for program

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,23 @@
 module Main where
 
-import Parser
+import qualified Data.Text.IO        as T
+import           Options.Applicative
+import           Parser
+
+newtype Filename = Filename { name :: String }
+
+fileArg :: Parser Filename
+fileArg = Filename <$> argument str (metavar "FILE")
+
+opts :: ParserInfo Filename
+opts = info (fileArg <**> helper)
+  (fullDesc
+   <> progDesc "Parse a PyHask file"
+   <> header "PyHask parser")
 
 main :: IO ()
-main = return ()
+main = do
+  fileData <- execParser opts
+  let filename = name fileData
+  input <- T.readFile filename
+  either putStrLn print $ parseProgram filename input

5e01f6fc0f1958c6a6fbd9fd72d2930f037afdbf
Jose Flores <josecruzflores.fl@gmail.com>
2020-04-18 21:03:53 -0500

    Add some expression checks

--- src/AnalysisUtils.hs
+++ src/AnalysisUtils.hs
@@ -1,16 +1,37 @@
 module AnalysisUtils where
 
 import           Control.Monad.State.Lazy
+import           Data.Foldable
 import           Data.List.NonEmpty       (NonEmpty)
 import qualified Data.List.NonEmpty       as N
 import qualified Data.Map.Strict          as M
 import           Data.Maybe
 import           Data.Text                (Text)
+import qualified Data.Text                as T
 import           ParserTypes
 
 
 --TODO: Actually do this
 exprCheck :: SimpleExpr -> Parser Expr
+exprCheck (Var ident) = do
+  (Variable vType initialized) <- findVariable ident
+  if initialized
+    then fail $ "Can't use uninitailized variable " ++ T.unpack ident
+  else
+    return (Expr (Var ident) vType)
+exprCheck (IntLiteral integer) = return (Expr (IntLiteral integer) (Simple IntType))
+exprCheck (FloatLiteral float) = return (Expr (FloatLiteral float) (Simple FloatType))
+exprCheck (BoolLiteral bool) = return (Expr (BoolLiteral bool) (Simple BoolType))
+exprCheck (Not sExpr) = do
+  (Expr cExpr cType) <- exprCheck sExpr
+  if cType == Simple BoolType
+    then return (Expr (Not cExpr) cType)
+    else fail "Only boolean expressions can be negated"
+exprCheck (Neg sExpr) = do
+  (Expr cExpr cType) <- exprCheck sExpr
+  if cType == Simple IntType || cType == Simple FloatType
+    then return (Expr (Neg cExpr) cType)
+    else fail "Only numeric values have a negative"
 exprCheck e = return $ Expr e (Simple IntType)
 
 existsIdentifier :: Text -> Parser Bool
@@ -34,6 +55,14 @@ existsVariable :: Text -> Parser Bool
 existsVariable variableIdentifier = do
     list <- gets scopes
     return (any (M.member variableIdentifier) (fmap scopeVariables list))
+
+maybeToParser :: String -> Maybe a -> Parser a
+maybeToParser e = maybe (fail e) return
+
+findVariable :: Text -> Parser Variable
+findVariable ident = do
+  maps <- fmap scopeVariables <$> gets scopes
+  maybeToParser ("Variable " ++ T.unpack ident ++ " not found") $ asum $ fmap (M.lookup ident) maps
 
 modifyScopes :: (NonEmpty Scope -> NonEmpty Scope) -> ParserState -> ParserState
 modifyScopes f (ParserState s d c) = ParserState (f s) d c

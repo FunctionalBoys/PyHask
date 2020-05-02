9969bdf18fe56efd67d25caee089481096d8ea3f
Jose Flores <josecruzflores.fl@gmail.com>
2020-05-02 11:17:15 -0500

    Add generation utilities

---
+++ src/GenUtils.hs
@@ -0,0 +1,26 @@
+module GenUtils where
+
+import           Control.Category
+import           Control.Monad.State.Lazy
+import qualified Data.Sequence            as S
+import           ParserTypes
+import           Utils
+
+quadruplesCounter :: ParserState -> Int
+quadruplesCounter (ParserState _ _ _ quads) = S.length quads
+
+addQuadruple :: Quad -> ParserState -> ParserState
+addQuadruple quad (ParserState ss fs cs quads) = ParserState ss fs cs (quads S.|> quad)
+
+updateQuadruple :: Int -> Quad -> ParserState -> ParserState
+updateQuadruple index quad (ParserState ss fs cs quads) = ParserState ss fs cs (S.update index quad quads)
+
+lookupQuadruple :: Int -> ParserState -> Maybe Quad
+lookupQuadruple i (ParserState _ _ _ quads) = quads S.!? i
+
+safeQuadrupleUpdate :: (Quad -> Either String Quad) -> Int -> Parser ()
+safeQuadrupleUpdate f index = do
+  mQuad <- gets $ lookupQuadruple index
+  quad <- maybeToParser ("No quadruple at index " ++ show index) mQuad
+  newQuad <- liftEither <<< f $ quad
+  modify $ updateQuadruple index newQuad

2dbaae739b8056cfd9e216fb8758c59ec9058c74
Jose Flores <josecruzflores.fl@gmail.com>
2020-05-02 15:33:25 -0500

    Remove unnecesary import

--- src/Utils.hs
+++ src/Utils.hs
@@ -1,14 +1,12 @@
 module Utils where
 
-import qualified Control.Monad.Fail as Fail
-
-maybeFail :: (Fail.MonadFail m) => String -> Maybe a -> m a
+maybeFail :: (MonadFail m) => String -> Maybe a -> m a
 maybeFail e = maybe (fail e) return
 
-guardFail :: (Fail.MonadFail m) => Bool -> String -> m ()
+guardFail :: (MonadFail m) => Bool -> String -> m ()
 guardFail True _  = return ()
-guardFail False s = Fail.fail s
+guardFail False s = fail s
 
-liftEither :: (Fail.MonadFail m) => Either String a -> m a
+liftEither :: (MonadFail m) => Either String a -> m a
 liftEither (Right a) = return a
-liftEither (Left e) = fail e
+liftEither (Left e)  = fail e

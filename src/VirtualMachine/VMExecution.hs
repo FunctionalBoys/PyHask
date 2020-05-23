b3b7f4d31bb2e6498dd45593942d018c02a140b9
Jose Flores <josecruzflores.fl@gmail.com>
2020-05-23 14:45:29 -0500

    Virtual machine utlities

--- src/VirtualMachine/VMExecution.hs
+++ src/VirtualMachine/VMExecution.hs
@@ -1,5 +1,8 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module VirtualMachine.VMExecution (virtualMachine) where
 
+import           Control.Category
 import           Control.Monad.Except
 import           Control.Monad.Reader
 import           Control.Monad.State.Lazy
@@ -20,7 +23,10 @@ nextInstruction = do
   maybe (throwError "Instruction pointer out of bounds") return mInstruction
 
 executionCondition :: VirtualMachine Bool
-executionCondition = (/= ProgramEnd) <$> nextInstruction
+executionCondition = isNotEnd <$> nextInstruction
+  where
+    isNotEnd ProgramEnd = False
+    isNotEnd _          = True
 
 increaseInstructionPointer :: Int -> MachineState -> MachineState
 increaseInstructionPointer increase mState@MachineState{instructionPointer=pointer} = mState{instructionPointer=pointer+increase}
@@ -36,3 +42,64 @@ executeInstruction ProgramEnd = throwError "Program end should be unreachable by
 
 virtualMachine :: VirtualMachine ()
 virtualMachine = whileM_ executionCondition executionAction
+
+addressConversion :: TypeBounds -> Address -> Either String Int
+addressConversion (TypeBounds vLower vUpper tLower tUpper) address
+  | address >= vLower && address <= vUpper = Right $ address - vLower
+  | address >= tLower && address <= tUpper = Right $ address - tLower + vUpper + 1
+  | otherwise = Left "Address is not convertable"
+
+getLocalContext :: VirtualMachine LocalContext
+getLocalContext = do
+  contexts <- gets localContexts
+  case contexts of
+    context : _ -> return context
+    []          -> throwError "Not inside a local context"
+
+addressContextType :: Address -> ContextType
+addressContextType address
+  | address >= 1 && address <= 12000 = Global
+  | address >= 12001 && address <= 44000 = Local
+  | otherwise = Static
+
+getValueFromIndex :: MachineType -> MemoryBlock -> Int -> Maybe TypeWrapper
+getValueFromIndex IntType MemoryBlock{..} index = IntWrapper <$> intMemory V.!? index
+getValueFromIndex FloatType MemoryBlock{..} index = FloatWrapper <$> floatMemory V.!? index
+getValueFromIndex CharType MemoryBlock{..} index = CharWrapper <$> charMemory V.!? index
+getValueFromIndex BoolType MemoryBlock{..} index = BoolWrapper <$> boolMemory V.!? index
+
+updateMemoryBlock :: Int -> TypeWrapper -> MemoryBlock -> MemoryBlock
+updateMemoryBlock index (IntWrapper int) mBlock@MemoryBlock{intMemory=memory} = mBlock{intMemory = memory V.// [(index,int)]}
+updateMemoryBlock index (FloatWrapper float) mBlock@MemoryBlock{floatMemory=memory} = mBlock{floatMemory = memory V.// [(index, float)]}
+updateMemoryBlock index (CharWrapper char) mBlock@MemoryBlock{charMemory=memory} = mBlock{charMemory = memory V.// [(index, char)]}
+updateMemoryBlock index (BoolWrapper bool) mBlock@MemoryBlock{boolMemory=memory} = mBlock{boolMemory = memory V.// [(index, bool)]}
+
+updateContext :: ContextType -> LocalContext -> MachineState -> MachineState
+updateContext Global context mState = mState{globalContext = context}
+updateContext Static context mState = mState{staticContext = context}
+updateContext Local context mState@MachineState{localContexts=_ : xs} = mState{localContexts = context : xs}
+updateContext _ _ mState = mState
+
+getContext :: ContextType -> VirtualMachine LocalContext
+getContext Local  = getLocalContext
+getContext Global = gets globalContext
+getContext Static = gets staticContext
+
+addressContext :: Address -> VirtualMachine LocalContext
+addressContext  = getContext <<< addressContextType
+
+getValueFromAddress :: Address -> VirtualMachine TypeWrapper
+getValueFromAddress address = do
+  LocalContext{localMemory=memory, addressType=addressConvertor} <- addressContext address
+  let (addressType, bounds) = addressConvertor address
+  index <- liftEither $ addressConversion bounds address
+  liftEither $ maybe (Left "Address out of type bounds") Right $ getValueFromIndex addressType memory index
+
+setValue :: TypeWrapper -> Address -> VirtualMachine ()
+setValue value address = do
+  let contextType = addressContextType address
+  localContext@LocalContext{localMemory=memory, addressType=addressConvertor} <- addressContext address
+  let (_, bounds) = addressConvertor address
+  index <- liftEither $ addressConversion bounds address
+  let nMemory = updateMemoryBlock index value memory
+  modify $ updateContext contextType localContext{localMemory=nMemory}
